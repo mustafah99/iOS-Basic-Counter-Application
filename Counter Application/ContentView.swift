3db10e671476434d76423900a6b849fe1ab7a7f3
Mustafa Said Helal <mustafasaidhelal@gmail.com>
2021-01-18 00:23:09 +0100

    Added Color Changer below Counter Buttons

--- Counter Application/ContentView.swift
+++ Counter Application/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State var count: Int = 0
     
+    @State var backgroundColor = LinearGradient(gradient: Gradient(colors: [Color.darkBlue, Color.shadeOfBlue]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.darkBlue, Color.shadeOfBlue]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+            backgroundColor
             
             VStack(alignment: .center, spacing:25) {
                 Spacer()
@@ -74,6 +76,51 @@ struct ContentView: View {
                 
                 Spacer()
             }
+            
+            HStack(spacing: 50) {
+                Button(action: {
+                    self.backgroundColor = LinearGradient(gradient: Gradient(colors: [Color.darkBlue, Color.red]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+                }) {
+                    Text("DBR")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 30))
+                        
+                        
+                }
+                
+                Button(action: {
+                    self.backgroundColor = LinearGradient(gradient: Gradient(colors: [Color.blue, Color.red]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+                }) {
+                    Text("RB")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 30))
+                        
+                        
+                }
+                
+                Button(action: {
+                    self.backgroundColor = LinearGradient(gradient: Gradient(colors: [Color.blue, Color.shadeOfBlue]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+                }) {
+                    Text("LB")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 30))
+                        
+                        
+                }
+                
+                Button(action: {
+                    self.backgroundColor = LinearGradient(gradient: Gradient(colors: [Color.darkBlue, Color.shadeOfBlue]), startPoint: /*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/)
+                }) {
+                    Text("DB")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 30))
+                        
+                        
+                }
+
+            }
+            .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/150.0/*@END_MENU_TOKEN@*/)
+            
         }
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         
